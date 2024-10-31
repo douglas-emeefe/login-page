e62303febd45a05c4521784a14f9664d9520d7b5
douglas-emeefe <douglasoliveira.main@gmail.com>
2024-10-31 16:51:59 -0400

    feat:create login and register screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { SingIn } from './app/SingIn.jsx';
+import { SingUp } from './app/SIngUp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SingIn />
   </StrictMode>,
 )
